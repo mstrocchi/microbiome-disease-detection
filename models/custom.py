a0be6f2c21470ade1a4687ee53616cf515260358
mstrocchi <matt.strocchi@gmail.com>
2022-06-02 10:31:59 +0200

    Introduce custom models

---
+++ models/custom.py
@@ -0,0 +1,49 @@
+from torch import nn
+from models.modules import ISAB, SAB, PMA
+
+
+class SetTransformerMultipleISAB(nn.Module):
+    def __init__(self, dim_input, num_outputs, dim_output,
+            num_inds=32, dim_hidden=128, num_heads=4, ln=False):
+        super(SetTransformerMultipleISAB, self).__init__()
+        self.enc = nn.Sequential(
+                ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
+                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln))
+        self.dec = nn.Sequential(
+                PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                nn.Linear(dim_hidden, dim_output))
+
+    def forward(self, X):
+        return self.dec(self.enc(X))
+
+
+class SetTransformerCustomDropout(nn.Module):
+    def __init__(
+        self,
+        dim_input=3,
+        num_outputs=1,
+        dim_output=40,
+        num_inds=32,
+        dim_hidden=128,
+        num_heads=4,
+        ln=False,
+        p=0.5
+    ):
+        super(SetTransformerCustomDropout, self).__init__()
+        self.enc = nn.Sequential(
+            ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
+            ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+        )
+        self.dec = nn.Sequential(
+            nn.Dropout(p=p),
+            PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+            nn.Dropout(p=p),
+            nn.Linear(dim_hidden, dim_output),
+        )
+
+    def forward(self, X):
+        return self.dec(self.enc(X))
